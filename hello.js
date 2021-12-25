b4a2b2056639b529cd4036e7916518c7d9d5c275
Tariq Mohammed Hamdi <thamdi@solutions.com.sa>
2021-12-25 12:50:23 +0300

    tarig 3

--- hello.js
+++ hello.js
@@ -5,6 +5,6 @@ var http = require('http');
 http.createServer(function (req, res) {
     // write the response and send it to the client
     res.writeHead(200, {'Content-Type': 'text/html'}); 
-    res.write('Node.js says hello! to Tarig 2');
+    res.write('Node.js says hello! to Tarig 3');
     res.end();
 }).listen(80);
